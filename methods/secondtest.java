abdbc61263d940c6cf668f52a711f1a4d5c9919b
2007srishreya-art <2007srishreya@gmail.com>
2026-05-29 16:54:09 +0530

    testing second

---
+++ methods/secondtest.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class secondtest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first num: ");
+        int a= sc.nextInt();
+        System.out.println("enter second num: ");
+        int b= sc.nextInt();
+        System.out.println("here is your output: ");
+        System.out.println(a/b);
+    }
+         
+    
+}
