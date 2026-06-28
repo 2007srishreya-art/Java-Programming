8a50173e049b06261789fa1267454930274ed6be
2007srishreya-art <2007srishreya@gmail.com>
2026-06-28 16:13:14 +0530

    Add pattern printing program using asterisks

---
+++ pattern printing/pattern.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("row= ");
+        int row= sc.nextInt();
+        System.out.print("column= ");
+        int column= sc.nextInt();
+
+        //outer loop
+        for(int i=1; i<=row; i++){
+            //inner loop
+            for(int j=1; j<=column; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        }
+    }
+    
+
